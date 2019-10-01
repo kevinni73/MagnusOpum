41ead5ef342b19893a43744d2dbe9616a4b40c1d
Kevin Ni <kevinni@umich.edu>
2019-09-30 20:52:37 -0700

    Cut out extra stuff

--- MagnusOpum/Components/Player.cs
+++ MagnusOpum/Components/Player.cs
@@ -14,25 +14,19 @@ using System.Threading.Tasks;
 namespace MagnusOpum.Components {
     class Player : Component, ITriggerListener, IUpdatable {
         public float moveSpeed = 300;
-        public float rollSpeed = 250;
         public float gravity = 1500;
         public float jumpHeight = 16 * 6;
 
         public float terminalVelocity = 500;
-        public float fastFallVelocity = 600;
 
         SpriteAnimator _animator;
         TiledMapMover _tileMover;
-        Mover _mover;
         BoxCollider _boxCollider;
         Vector2 _velocity;
         TiledMapMover.CollisionState _collisionState = new TiledMapMover.CollisionState();
-        FollowCamera _followCam;
         Dictionary<string, SpriteAnimator.LoopMode> _animatorLoop = new Dictionary<string, SpriteAnimator.LoopMode>();
 
         VirtualButton _jumpInput;
-        VirtualButton _attackInput;
-        VirtualButton _rollInput;
         VirtualIntegerAxis _xAxisInput;
         VirtualIntegerAxis _yAxisInput;
 
@@ -43,10 +37,8 @@ namespace MagnusOpum.Components {
             var sprites = Sprite.SpritesFromAtlas(texture, 50, 37);
 
             _tileMover = Entity.GetComponent<TiledMapMover>();
-            _mover = Entity.GetComponent<Mover>();
             _boxCollider = Entity.GetComponent<BoxCollider>();
             _animator = Entity.AddComponent(new SpriteAnimator(sprites[0]));
-            _followCam = Entity.GetComponent<FollowCamera>();
 
             _animator.AddAnimation("idle", 6, new[]{
                 sprites[38],
@@ -77,51 +69,14 @@ namespace MagnusOpum.Components {
                 sprites[23],
             });
 
-            _animator.AddAnimation("attack", 15, new [] {
-                //sprites[47], lower startup
-                sprites[48],
-                sprites[49],
-                sprites[50],
-                sprites[51],
-                sprites[52],
-            });
-            _animatorLoop["attack"] = SpriteAnimator.LoopMode.Once;
-
-            _animator.AddAnimation("roll", 15, new [] {
-                sprites[18],
-                sprites[19],
-                sprites[20],
-                sprites[21],
-                sprites[18],
-                sprites[19],
-            });
-            _animatorLoop["roll"] = SpriteAnimator.LoopMode.Once;
-
-            _animator.AddAnimation("crouch", new [] {
+            _animator.AddAnimation("crouch", new[] {
                 sprites[4],
                 sprites[5],
                 sprites[6],
                 sprites[7],
             });
 
-            _animator.OnAnimationCompletedEvent += onFinishAnimation;
-
             setupInput();
-        }
-
-        void onFinishAnimation(string animation) {
-            if (animation == "attack") {
-                _animator.Play("idle");
-            }
-            else if (animation == "roll") {
-                _boxCollider.SetHeight(32);
-                var _ = new Vector2();
-                _tileMover.TestCollisions(ref _, _boxCollider.Bounds, _collisionState);
-                if (_collisionState.Above) {
-                    _boxCollider.SetHeight(16);
-                    _animator.Play("crouch");
-                }
-            }
         }
 
         string transitionAnimation(string previous, string next) {
@@ -129,39 +84,12 @@ namespace MagnusOpum.Components {
                 return next;
             }
             if (previous == "crouch") {
-                _boxCollider.SetLocalOffset(new Vector2(_boxCollider.LocalOffset.X, 0));
-
-                if (next != "roll") {
-                    _boxCollider.SetHeight(32);
-                    var _ = new Vector2();
-                    _tileMover.TestCollisions(ref _, _boxCollider.Bounds, _collisionState);
-                    if (_collisionState.Above) {
-                        _boxCollider.SetHeight(16);
-                        _boxCollider.SetLocalOffset(new Vector2(_boxCollider.LocalOffset.X, 8));
-                        return "crouch";
-                    }
-                }
-            }
-            else if (previous == "roll") {
-                _boxCollider.Transform.Position = new Vector2(_boxCollider.Transform.Position.X, _boxCollider.Transform.Position.Y - 8);
                 _boxCollider.SetHeight(32);
-                var _ = new Vector2();
-                _tileMover.TestCollisions(ref _, _boxCollider.Bounds, _collisionState);
-                if (_collisionState.Above) {
-                    _boxCollider.SetHeight(16);
-                    _boxCollider.SetLocalOffset(new Vector2(_boxCollider.LocalOffset.X, 8));
-                    return "crouch";
-                }
+                _boxCollider.SetLocalOffset(new Vector2(_boxCollider.LocalOffset.X, 0));
             }
-
             if (next == "crouch") {
                 _boxCollider.SetHeight(16);
                 _boxCollider.SetLocalOffset(new Vector2(_boxCollider.LocalOffset.X, 8));
-            }
-            else if (next == "roll") {
-                _boxCollider.Transform.Position = new Vector2(_boxCollider.Transform.Position.X, _boxCollider.Transform.Position.Y + 8);
-                _boxCollider.SetHeight(16);
-                _boxCollider.SetLocalOffset(new Vector2(_boxCollider.LocalOffset.X, 0));
             }
 
             return next;
@@ -174,8 +102,6 @@ namespace MagnusOpum.Components {
             _xAxisInput.Deregister();
             _yAxisInput.Deregister();
             _jumpInput.Deregister();
-            _attackInput.Deregister();
-            _rollInput.Deregister();
         }
 
         void setupInput() {
@@ -183,12 +109,6 @@ namespace MagnusOpum.Components {
             _jumpInput = new VirtualButton();
             _jumpInput.Nodes.Add(new Nez.VirtualButton.KeyboardKey(Keys.Space));
             _jumpInput.Nodes.Add(new Nez.VirtualButton.GamePadButton(0, Buttons.A));
-
-            _attackInput = new VirtualButton();
-            _attackInput.Nodes.Add(new Nez.VirtualButton.GamePadButton(0, Buttons.X));
-
-            _rollInput = new VirtualButton();
-            _rollInput.Nodes.Add(new Nez.VirtualButton.GamePadButton(0, Buttons.B));
 
             // horizontal input from dpad, left stick or keyboard left/right
             _xAxisInput = new VirtualIntegerAxis();
@@ -206,89 +126,58 @@ namespace MagnusOpum.Components {
         void IUpdatable.Update() {
             // handle movement and animations
             Vector2 moveDir = new Vector2(_xAxisInput.Value, _yAxisInput.Value);
-            string animation = _animator.CurrentAnimationName;
-
-            if (_animator.CurrentAnimationName == "attack") {
-                return;
-            }
-
-            if (_animator.CurrentAnimationName == "roll" && _animator.CurrentFrame != _animator.CurrentAnimation.Sprites.Count() - 1) {
-                if (_animator.FlipX) {
-                    _velocity.X = -rollSpeed;
-                }
-                else {
-                    _velocity.X = rollSpeed;
+            string animation = null;
+            
+            if (moveDir.Y > 0) {
+                if (_collisionState.Below) {
+                    animation = "crouch";
+                    if (moveDir.X < 0) {
+                        _animator.FlipX = true;
+                    }
+                    else {
+                        _animator.FlipX = false;
+                    }
+                    _velocity.X = 0;
                 }
             }
             else {
-                if (_rollInput.IsPressed && _collisionState.Below) {
-                    if (_animator.FlipX) {
-                        _velocity.X = -moveSpeed;
+                if (moveDir.X < 0) {
+                    if (_collisionState.Below) {
+                        animation = "run";
                     }
-                    else {
-                        _velocity.X = moveSpeed;
-                    }
-
-                    animation = "roll";
+                    _animator.FlipX = true;
+                    _velocity.X = -moveSpeed;
                 }
-                else if (_attackInput.IsPressed && _collisionState.Below) {
-                    animation = "attack";
+                else if (moveDir.X > 0) {
+                    if (_collisionState.Below) {
+                        animation = "run";
+                    }
+                    _animator.FlipX = false;
+                    _velocity.X = moveSpeed;
                 }
                 else {
-                    if (moveDir.Y > 0) {
-                        if (_collisionState.Below) {
-                            animation = "crouch";
-                            if (moveDir.X < 0) {
-                                _animator.FlipX = true;
-                            }
-                            else {
-                                _animator.FlipX = false;
-                            }
-                        }
-                        else if (_animator.CurrentAnimationName == "falling") {
-                            _velocity.Y = fastFallVelocity;
-                        }
-                    }
-                    else {
-                        if (moveDir.X < 0) {
-                            if (_collisionState.Below) {
-                                animation = "run";
-                            }
-                            _animator.FlipX = true;
-                            _velocity.X = -moveSpeed;
-                        }
-                        else if (moveDir.X > 0) {
-                            if (_collisionState.Below) {
-                                animation = "run";
-                            }
-                            _animator.FlipX = false;
-                            _velocity.X = moveSpeed;
-                        }
-                        else {
-                            _velocity.X = 0;
-                            if (_collisionState.Below) {
-                                animation = "idle";
-                            }
-                        }
-                    }
-
-                    if (_collisionState.Below && _jumpInput.IsPressed) {
-                        animation = "jump";
-                        jumpKeyHeld = true;
-                        _velocity.Y = -Mathf.Sqrt(2f * jumpHeight * gravity);
-                    }
-                    else if (_jumpInput.IsReleased) {
-                        jumpKeyHeld = false;
-                    }
-
-                    if (_collisionState.Above) {
-                        _velocity.Y = 0;
-                    }
-
-                    if (!_collisionState.Below & _velocity.Y > 0) {
-                        animation = "falling";
+                    _velocity.X = 0;
+                    if (_collisionState.Below) {
+                        animation = "idle";
                     }
                 }
+            }
+
+            if (_collisionState.Below && _jumpInput.IsPressed) {
+                animation = "jump";
+                jumpKeyHeld = true;
+                _velocity.Y = -Mathf.Sqrt(2f * jumpHeight * gravity);
+            }
+            else if (_jumpInput.IsReleased) {
+                jumpKeyHeld = false;
+            }
+
+            if (_collisionState.Above) {
+                _velocity.Y = 0;
+            }
+
+            if (!_collisionState.Below & _velocity.Y > 0) {
+                animation = "falling";
             }
 
             if (_animator.CurrentAnimationName == "jump" && !jumpKeyHeld) {
@@ -304,13 +193,7 @@ namespace MagnusOpum.Components {
             }
 
             animation = transitionAnimation(_animator.CurrentAnimationName, animation);
-            if (animation != "crouch") {
-                // move
-                Vector2 movement = _velocity * Time.DeltaTime;
-                CollisionResult collisionResult;
-                _tileMover.TestCollisions(ref movement, _boxCollider.Bounds, _collisionState);
-                _mover.Move(movement, out collisionResult);
-            }
+            _tileMover.Move(_velocity * Time.DeltaTime, _boxCollider, _collisionState);
 
             // reset gravity acceleration
             if (_collisionState.Below) {
